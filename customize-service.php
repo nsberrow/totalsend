920e6722aa31adafa86d4ec4abdd531d3009ccae
Neil Berrow <neil.berrow@takealot.com>
2016-10-06 22:44:51 +0200

    Rough prototype

--- customize-service.php
+++ customize-service.php
@@ -17,9 +17,9 @@
               </ul>
               <div class="indicator-line">
                 <ul>
-                  <li></li>
-                  <li></li>
-                  <li></li>
+                  <li class="first-dot"></li>
+                  <li class="second-dot"></li>
+                  <li class="third-dot"></li>
                 </ul>
               </div>
             </div>
@@ -30,17 +30,39 @@
             <div class="col-sm-4">
             </div>
             <div class="col-sm-4 text-center">
-              <p class="lead">Select your sector</p>
-              <ul class="selector" id="selector">
+              <p class="lead" id="customize-title">Select your sector</p>
+              <ul class="selector" id="sector">
                 <li><input type="checkbox" id="banking">BANKING</li>
                 <li><input type="checkbox" id="marketing">MARKETING</li>
                 <li><input type="checkbox" id="medical">MEDICAL</li>
                 <li><input type="checkbox" id="government">GOVERNMENT</li>
                 <li><input type="checkbox" id="other">OTHER</li>
               </ul>
+              <ul class="selector" id="channel">
+                <li><input type="checkbox" id="marketing-email">MARKETING EMAIL</li>
+                <li><input type="checkbox" id="sms">SMS</li>
+                <li><input type="checkbox" id="transactional-email">TRANSACTIONAL EMAIL</li>
+                <li><input type="checkbox" id="push">PUSH</li>
+              </ul>
+              <div class="signup-form">
+
+              </div>
             </div>
             <div class="col-sm-4">
             </div>
+        </div>
+        <div class="row">
+          <div class="col-sm-4">
+          </div>
+          <div class="col-sm-4">
+            <a class="btn btn--sm next-btn">
+              <span class="btn__text customize-btn-title">
+                 Next, Channels →
+              </span>
+            </a>
+          </div>
+          <div class="col-sm-4">
+          </div>
         </div>
     </div>
 </section>
--- js/customizer.js
+++ js/customizer.js
@@ -1,13 +1,29 @@
-var banking = $('#banking'),
-    marketing = $('#marketing'),
-    medical = $('#medical'),
-    government = $('#government'),
-    other = $('#other');
+var title = $('#customize-title'),
+    btnWrapper = $('.next-btn'),
+    btn = $('.customize-btn-title'),
+    dot1 = $('.indicator-line li.first-dot'),
+    dot2 = $('.indicator-line li.second-dot'),
+    dot3 = $('.indicator-line li.third-dot'),
+    array  = [];
 
-$("#selector li input").change(function() {
+$(".selector li input").change(function() {
   if(this.checked) {
-    //var checkboxId = this.attr('id');
-    var item = validate($(this));
-    console.log(item.attr('id'));
+    var checked = $(this).attr('id');
+    btnWrapper.css('display', 'block');
+    btn.text("Next, channels →");
+
+    btn.on('click', function(){
+      $('#sector').css('display', 'none').attr('id', '1');
+      $('#channel').css('display', 'block').attr('id', '2');
+      $('.signup-form').attr('id', '3');
+  
+      dot2.css('background', 'white');
+      array.push(checked);
+      console.log(array);
+    });
+
+    // if (checked.length >= 1){
+    //
+    // }
   }
 });
